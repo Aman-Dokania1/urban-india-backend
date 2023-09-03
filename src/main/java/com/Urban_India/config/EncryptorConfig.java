eff7ed851504c755a38b0b34a1a8e28194fa1ba0
Aman Dokania <gupta.aman0203@gmail.com>
2023-09-03 14:01:53 +0530

    added jasypt algo

--- src/main/java/com/Urban_India/config/EncryptorConfig.java
+++ src/main/java/com/Urban_India/config/EncryptorConfig.java
@@ -1,4 +1,32 @@
 package com.Urban_India.config;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
 public class EncryptorConfig {
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor getSensitiveInfoEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("Urban_India");// private -key
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations(1000);
+        config.setPoolSize(1);
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+            System.out.println(encryptor.encrypt("postgres"));
+        return encryptor;
+    }
+
+        public static void main(String[] args) {
+        new EncryptorConfig().getSensitiveInfoEncryptor();
+    }
 }
